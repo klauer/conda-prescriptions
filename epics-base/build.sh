3015f0513d77e85c90b62113fb71911332a6f2c3
Ken L <klauer@bnl.gov>
2015-01-14 10:48:56 -0500

    Host arch for epics-base copy; pyepics dash in version number

--- epics-base/build.sh
+++ epics-base/build.sh
@@ -5,22 +5,24 @@ if [ "$(uname)" == "Darwin" ]; then
     export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib"
     # TODO cp flags differ, -P is default with -r, but can't be specified somehow
     CP_FLAGS="-rv"
+    HOST_ARCH=darwin-x86
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     CP_FLAGS="-Prv"
+    HOST_ARCH=linux-x86
 fi
 
 make -j$(getconf _NPROCESSORS_ONLN)
 EPICS_BASE=$PREFIX/lib/epics
 
 install -d $PREFIX/lib
-cp $CP_FLAGS lib/darwin-x86/* $PREFIX/lib
+cp $CP_FLAGS lib/$HOST_ARCH/* $PREFIX/lib
 
 install -d $PREFIX/bin
-cp $CP_FLAGS bin/darwin-x86/caget $PREFIX/bin
-cp $CP_FLAGS bin/darwin-x86/caput $PREFIX/bin
-cp $CP_FLAGS bin/darwin-x86/camonitor $PREFIX/bin
-cp $CP_FLAGS bin/darwin-x86/caRepeater $PREFIX/bin
-cp $CP_FLAGS bin/darwin-x86/softIoc $PREFIX/bin
+cp $CP_FLAGS bin/$HOST_ARCH/caget $PREFIX/bin
+cp $CP_FLAGS bin/$HOST_ARCH/caput $PREFIX/bin
+cp $CP_FLAGS bin/$HOST_ARCH/camonitor $PREFIX/bin
+cp $CP_FLAGS bin/$HOST_ARCH/caRepeater $PREFIX/bin
+cp $CP_FLAGS bin/$HOST_ARCH/softIoc $PREFIX/bin
 
 for X in bin configure db dbd include lib startup templates; do
   install -d $EPICS_BASE/$X
